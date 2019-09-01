89923205df7a0818c55bb539516658dac5907390
hollandjb <hollandjb@vcu.edu>
2019-09-01 18:42:48 -0400

    updated our directory items to navigate with Router HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import "./App.css";
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 function App() {
   return (
-    <div className="App">
-      <HomePage />
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 import Directory from "../../components/directory/directory.component";
-import MenuItem from "../../components/menu-item/menu-item.component";
 
 import "./homepage.styles.scss";
 
