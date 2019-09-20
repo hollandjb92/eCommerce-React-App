3256e95814b306ae691fbeadb5c19b59d9fd8f45
hollandjb <hollandjb@vcu.edu>
2019-09-19 20:09:26 -0400

    heroku deployment

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express"),
   cors = require("cors"),
   path = require("path"),
   app = express(),
-  PORT = 5000 || process.env.PORT;
+  PORT = process.env.PORT || 5000;
 
 
 if (process.env.NODE_ENV !== "production") {
